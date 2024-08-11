136c8fc39d6ae2c1d3a06bf2f285135512313df2
aniss-47 <officialanish172@gmail.com>
2024-08-11 17:34:09 +0545

    Print function in python

---
+++ pythondaily/print.py
@@ -0,0 +1,22 @@
+#------------------String------------------------#
+# Note: You should use quotation to print string
+
+print ("Hello World")
+
+#You can use any single or double quotation signs
+
+print ('Hello World')
+
+#Error : When you dont use quotation to print string
+
+
+#----------------------Integer---------------------#
+#Note: You don't need quotation to print integer
+
+print (77)
+print(4)
+
+#If you use quotation to print integer, the integer becomes string.
+
+print ('44')
+print ("98")
