7106e038c77ceebc1266870ffdb9f4adbc334601
aniss-47 <officialanish172@gmail.com>
2024-09-08 19:33:01 +0545

    Learn Python

---
+++ pythondaily/callvariables.py
@@ -0,0 +1,25 @@
+#Calling variables
+
+name = "ronaldo"
+number = 7
+print (name)
+print (number)
+
+# Method 1 
+budget = 10
+print (budget + 10)
+
+# Method 2
+score = 10*7
+print (score)
+
+#Method 3
+salary = 900
+new_salary = salary + 200
+print(new_salary)
+
+#Method 4
+first_name = "cristiano"
+second_name = 'ronaldo'
+print ( first_name + " " + second_name)
+
---
+++ pythondaily/variable.py
@@ -0,0 +1,11 @@
+# variables - numbers,letters,underscores
+
+#We can use both single and double quotation to store variable
+message = "Hello World"
+print (message)
+
+message = 'Hi Everyone'
+print (message)
+
+numm = 1 
+print (numm)
