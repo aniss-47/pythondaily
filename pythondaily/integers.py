9bd6fddfa68fd29559c4b62183812573700163ef
aniss-47 <officialanish172@gmail.com>
2024-09-10 15:26:17 +0545

    Integeres in Python

---
+++ pythondaily/integers.py
@@ -0,0 +1,12 @@
+#integers are numbers
+
+print ( 2 + 2) #Adiition
+print (3*4) #Subtraction
+print(3/2)  #Division
+print(10/2) #Integer divided by integer always gives float.
+
+# Order
+print ( 2 + 3 * 4)
+
+# To break order you must use paranthesis.
+print ( (2+3)*4 )
