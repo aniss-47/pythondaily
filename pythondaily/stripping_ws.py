f146a62be27c23c6079cf90fb8b1eebc6bf0a9fd
aniss-47 <officialanish172@gmail.com>
2024-09-10 13:16:14 +0545

    Stripping whhitespaces

---
+++ pythondaily/stripping_ws.py
@@ -0,0 +1,16 @@
+#Stripping whitespace
+
+#lstrip
+fav_sport = "  football  "
+fav_sport= fav_sport.lstrip()
+print(fav_sport)
+
+#rstrip
+fav_sport = "  basketball  "
+fav_sport= fav_sport.rstrip()
+print(fav_sport)
+
+#strip
+fav_sport = "  rugby  "
+fav_sport= fav_sport.strip()
+print(fav_sport)
