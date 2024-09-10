3ca502394ebecf73f0d19228bf7774c7d1014097
aniss-47 <officialanish172@gmail.com>
2024-09-10 12:50:59 +0545

    F-string

---
+++ pythondaily/f-strings.py
@@ -0,0 +1,18 @@
+
+f_name = "gareth"
+l_name = "barry"
+full_name = f"{f_name} {l_name}"
+print (full_name)
+
+# f-strings to compose complete messages
+f_name = "ava"
+l_name = "rodriguez"
+full_name = f"{f_name} {l_name}"
+print(f"Hello, {full_name.title()}!")
+
+# or
+message = f"Hello,  {full_name.title()}!"
+print(message)
+
+
+
