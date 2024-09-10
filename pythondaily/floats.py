ab4808b4de339d39989cb36fce22c4fd6842b1b2
aniss-47 <officialanish172@gmail.com>
2024-09-10 15:30:56 +0545

    Decimals in Python

---
+++ pythondaily/floats.py
@@ -0,0 +1,10 @@
+#Floats are numbers with decimals.
+
+print ( 0.1 + 0.1) #Addition
+print ( 0.2 *0.4) #Multiplication
+print (0.8 / 0.2) #Division
+print (4.2 - 2.3) #Subtraction
+
+#Mixing integer and float in any operation gives you float.
+
+print (4 *1.0)
